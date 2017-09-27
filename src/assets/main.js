a3a74d187c1caaf79dfa18554dc6b0617ce21443
Yahia Thakib <yahia.thakib@d-velop.de>
2017-09-27 09:23:04 +0200

    newfunction

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,7 +3,39 @@ let attempt = document.getElementById('attempt');
 
 function guess() {
     let input = document.getElementById('user-guess');
-    //add functionality to guess function here
+    if(answer.value == '' || attempt.value == '') {
+        setHiddenFields();
+    }
+
+    if(validateInput(input.value)) {
+        return;
+    }
+    attempt.value++;
+}
+function getResults(input) {
+    let html = '<div class="row"><spn class="class="col-md-6">' + input + '</span><div class="col-md-6">';
+    for(i = 0; i < input.length; i++) {
+        if(input.charAt(i) == answer.value.charAt(i)) {
+            html += '<spn class=glyphicon glyphicon-ok"></span>';
+        }
+    }
 }
 
-//implement new functions here
+function setHiddenFields () {
+    answer.value = Math.floor(Math.random() *1000).toString();
+    weil(answer.value.length < 4) {
+        answer.value = "0" + answer.value;
+    }
+    attempt.value = "0";
+}
+
+function setMessage(message) {
+    document.getElementById('message').innerHTML = message;
+}
+function validateInput (input) {
+    if(input.length != 4) {
+        setMessage('Guesses Must be exactly 4 charactors long.');
+        return false;
+    }
+    return true;
+}
